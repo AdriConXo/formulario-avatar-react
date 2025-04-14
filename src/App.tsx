ebb589a31bb079f2c4630dbe8855aa7393d38abc
Israel <cursos.israelsp@gmail.com>
2025-04-14 20:26:26 +0200

    Actualizo nova rama

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,23 @@ import './App.css';
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [isUploading, setIsUploading] = useState(false);
-
+  const [withInputs,setInputs] = useState("")
   // Maneja la selección del archivo
   const handleFileChange = (event: any) => {
     console.log("Collo datos ? ",event.target.files[0])
     setSelectedFile(event.target.files[0]);
   };
+  const handleInputs = (event:any) =>{
+    console.log("event.target.value ",event.target.value)
+
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+    console.log("withInputs ",withInputs)
+  }
   // Maneja la subida del archivo
-  const handleUpload = async () => {
+  const handleUpload = async (e:any) => {
+    e.preventDefault()
     if (!selectedFile) {
       alert('Por favor, selecciona un archivo antes de subirlo.');
       return;
@@ -20,6 +29,13 @@ function App() {
     const formData = new FormData();
     formData.append('avatar', selectedFile);
     console.log('formData ?',formData)
+    console.log("withInputs en Uploada",withInputs)
+    for(let datos in withInputs){
+
+      console.log("datos ? ",withInputs[datos])
+
+      formData.append(`${datos}`,withInputs[datos])
+    }
     try {
       setIsUploading(true);
       let obxetoEnviado = {
@@ -44,10 +60,16 @@ function App() {
     <>
       <div>
         <h2>Subida de Archivos con React y Hooks</h2>
-        <input type="file" name="avatar" onChange={handleFileChange} />
-        <button onClick={handleUpload} disabled={isUploading}>
-          {isUploading ? 'Subiendo...' : 'Subir Archivo'}
-        </button>
+        <form method="post" onSubmit={handleUpload}>
+          <input type='text' name="dato1" placeholder='Introduce dato1' onChange={handleInputs}/>
+          <input type='text' name="dato2" placeholder='Introduce dato2' onChange={handleInputs}/>
+          <input type="file" name="avatar" onChange={handleFileChange} />
+          <button type='submit'>Enviar</button>
+          {/* <button onClick={handleUpload} disabled={isUploading}>
+            {isUploading ? 'Subiendo...' : 'Subir Archivo'}
+          </button> */}
+        </form>
+        
       </div>
     </>
   );
