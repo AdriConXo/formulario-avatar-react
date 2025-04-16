6a1bba2f836c43a042607a4562082ed620ebf4f3
Israel <cursos.israelsp@gmail.com>
2025-04-16 16:40:03 +0200

    Actualizando
    Acabo de insertar un icono de user, ademáis fago para que o clicar sobre a imaxen esta se volva abrir.
    Aprobeito a etiqueta 'label' para iso

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import FormularioRexistroUser from './componentes/FormularioRexistroUser';
-
+import iconoUser from './assets/cuenta.png';
 function App() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [selecteURL,setSelectedURL] = useState<string | null>(null);
@@ -19,6 +19,7 @@ function App() {
    
     if (event.target.files?.[0]) {
       setSelectedFile(event.target.files[0]);
+      console.log("URL.createObjectURL(event.target.files[0])",URL.createObjectURL(event.target.files[0]))
       // AGREGO IMAXEN CONVERTIDA
       setSelectedURL(URL.createObjectURL(event.target.files[0]));
     }
@@ -63,15 +64,21 @@ function App() {
     }
   };
 
+
+  
+
   return (
-    <div>
-      <input type='file' onChange={cambioImaxen} />
+    <div className='estilo-wrapper'>
+    <label>
       {
         // ### AGREGO IMAXEN CONVERTIDA
         // SE NON TEÑO IMAXEN --> <div>Ningunha imaxen seleccionada</div>
-        selecteURL != null ? <img src={selecteURL} /> : <div>Ningunha imaxen seleccionada</div>
+        selecteURL != null ? <img src={selecteURL} /> : <div><img src={iconoUser}/><div>Selecciona unha imaxen</div></div>
       }
-      <h2>Subida de Archivos con React y Hooks</h2>
+      <input className="input-transparente" type='file' onChange={cambioImaxen} />
+    </label>
+      
+      {/* <h2>Subida de Archivos con React y Hooks</h2> */}
       <FormularioRexistroUser
        handleFileChange={handleFileChange} 
        handleInputs={handleInputs}
